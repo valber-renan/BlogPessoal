cf21b8697dc7480743f1a1f6f70721f2d6f4045b
unknown <valber_05@hotmail.com>
2020-10-07 14:53:18 -0300

    Blog com crud

---
+++ blog_Pessoal/src/main/java/org/generation/blog_Pessoal/repository/TemaRepository.java
@@ -0,0 +1,10 @@
+package org.generation.blog_Pessoal.repository;
+
+import java.util.List;
+
+import org.generation.blog_Pessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByDescricaoContainingIgnoreCase( String descricao);
+}
