50350df10b8ce1b922413e3cf0d333c480f3f8df
unknown <helder825@hotmail.com>
2020-10-15 10:16:05 -0300

    security implements

---
+++ blog_Pessoal/src/main/java/org/generation/blog_Pessoal/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package org.generation.blog_Pessoal.repository;
+
+import java.util.Optional;
+
+import org.generation.blog_Pessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario>findByUsuario(String usuario);
+}
