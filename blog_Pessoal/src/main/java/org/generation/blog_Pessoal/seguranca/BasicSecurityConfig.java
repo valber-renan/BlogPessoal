b4a361d8a7b3a951128613375a9bfbae6d43505b
unknown <valber_05@hotmail.com>
2020-10-15 11:01:30 -0300

    seguranca no projeto

--- blog_Pessoal/src/main/java/org/generation/blog_Pessoal/seguranca/BasicSecurityConfig.java
+++ blog_Pessoal/src/main/java/org/generation/blog_Pessoal/seguranca/BasicSecurityConfig.java
@@ -35,5 +35,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
+		
 	}
 }
