4e5d522c5d0dc3f11fc05a80bff8b797a3a06fc7
unknown <valber_05@hotmail.com>
2020-10-18 14:38:27 -0300

    atualização

--- blog_Pessoal/src/main/java/org/generation/blog_Pessoal/controller/UsuarioController.java
+++ blog_Pessoal/src/main/java/org/generation/blog_Pessoal/controller/UsuarioController.java
@@ -29,8 +29,13 @@ public class UsuarioController {
 	}
 	
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario>Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
+		try {
+				return ResponseEntity.ok(user.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+		
 	}
 }
